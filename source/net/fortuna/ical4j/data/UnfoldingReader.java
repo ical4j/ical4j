8f403362b76ff2a0e815b9c24c0d0e2745b2da68
fortuna <fortuna@users.sourceforge.net>
2005-02-22 09:25:37 +0000

    Improved support for non-conformant folding

--- source/net/fortuna/ical4j/data/UnfoldingReader.java
+++ source/net/fortuna/ical4j/data/UnfoldingReader.java
@@ -1,7 +1,7 @@
 /*
  * $Id$ [06-Apr-2004]
  *
- * Copyright (c) 2004, Ben Fortuna
+ * Copyright (c) 2005, Ben Fortuna
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -22,7 +22,7 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -36,88 +36,84 @@ package net.fortuna.ical4j.data;
 import java.io.IOException;
 import java.io.PushbackReader;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
  * A reader which performs iCalendar unfolding as it reads.
+ * Note that unfolding rules may be "relaxed" to allow unfolding of
+ * non-conformant *.ics files. By specifying the system property
+ * "ical4j.unfolding.relaxed=true" iCalendar files created with
+ * Mozilla Calendar/Sunbird may be correctly unfolded.
  *
- * @author benfortuna
+ * @author Ben Fortuna
  */
 public class UnfoldingReader extends PushbackReader {
 
     private static Log log = LogFactory.getLog(UnfoldingReader.class);
 
     /**
-     * The pattern used to identify a fold in the iCalendar stream.
-     * The unfolder may use a relaxed version of the pattern which does
-     * not require the carriage return by specified the system property:
-     * <code>ical4j.unfolding.relaxed=true</code>
+     * The pattern used to identify a fold in an iCalendar data stream.
      */
-    private static final String FOLD_PATTERN = ("true".equals(System
-            .getProperty("ical4j.unfolding.relaxed"))) ? "\n " : "\r\n ";
+    private static final char[] DEFAULT_FOLD_PATTERN = {'\r', '\n', ' '};
+    
+    /**
+     * The pattern used to identify a fold in Mozilla Calendar/Sunbird
+     * and KOrganizer.
+     */
+    private static final char[] RELAXED_FOLD_PATTERN = {'\n', ' '};
+    
+    private static final List FOLD_PATTERNS = new ArrayList();
+    
+    static {
+        FOLD_PATTERNS.add(DEFAULT_FOLD_PATTERN);
+        if ("true".equals(System.getProperty("ical4j.unfolding.relaxed"))) {
+            FOLD_PATTERNS.add(RELAXED_FOLD_PATTERN);
+        }
+    }
 
-    private static final char[] CHAR_FOLD_PATTERN = FOLD_PATTERN.toCharArray();
-
-    private static final int BUFFER_SIZE = FOLD_PATTERN.length();
-
-    private char[] buffer = new char[BUFFER_SIZE];
+    private List buffers;
 
     /**
-     * @param in
-     *            a reader to read from
+     * @param in a reader to read from
      */
     public UnfoldingReader(final Reader in) {
-
-        super(in, BUFFER_SIZE);
+        super(in, DEFAULT_FOLD_PATTERN.length);
+        buffers = new ArrayList();
+        for (Iterator i = FOLD_PATTERNS.iterator(); i.hasNext();) {
+            char[] pattern = (char[]) i.next();
+            buffers.add(new char[pattern.length]);
+        }
     }
 
     /**
      * @see java.io.PushbackReader#read()
      */
     public final int read() throws IOException {
-
-        int read = super.read(buffer);
-
-        if (read > 0) {
-            //if (!FOLD_PATTERN.equals(new String(buffer, 0, read))) {
-            if (!Arrays.equals(CHAR_FOLD_PATTERN, buffer)) {
-                unread(buffer, 0, read);
-            }
-            else {
-                if (log.isDebugEnabled()) {
-                    log.debug("Unfolding..");
+        for (int i = 0; i < buffers.size(); i++) {
+            char[] buffer = (char[]) buffers.get(i);
+            int read = super.read(buffer);
+            if (read > 0) {
+                if (!Arrays.equals((char[]) FOLD_PATTERNS.get(i), buffer)) {
+                    unread(buffer, 0, read);
+                }
+                else {
+                    if (log.isDebugEnabled()) {
+                        log.debug("Unfolding..");
+                    }
+                    // return as soon as unfolding occurs..
+                    return super.read();
                 }
             }
-
-            return super.read();
+            else {
+                return read;
+            }
         }
-        
-        return read;
+        return super.read();
     }
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see java.io.Reader#read(char[])
-     *
-     * public int read(char[] arg0) throws IOException {
-     *
-     * super.read(buffer);
-     *
-     * if (!FOLD_PATTERN.equals(new String(buffer))) { unread(buffer); }
-     *
-     * return super.read(arg0); } /* (non-Javadoc)
-     * @see java.io.PushbackReader#read(char[], int, int)
-     *
-     * public int read(char[] arg0, int arg1, int arg2) throws IOException {
-     *
-     * super.read(buffer);
-     *
-     * if (!FOLD_PATTERN.equals(new String(buffer))) { unread(buffer); }
-     *
-     * return super.read(arg0, arg1, arg2); }
-     */
 }
