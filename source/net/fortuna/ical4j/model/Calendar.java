e00425843deaabf74917a42ebf74c72273155e76
fortuna <fortuna@users.sourceforge.net>
2005-04-07 10:03:03 +0000

    Minor updates

--- source/net/fortuna/ical4j/model/Calendar.java
+++ source/net/fortuna/ical4j/model/Calendar.java
@@ -47,6 +47,55 @@ import net.fortuna.ical4j.util.DateRangeNormalizer;
 /**
  * Defines an iCalendar calendar.
  * 
+ * <pre>
+ *   4.6 Calendar Components
+ *   
+ *      The body of the iCalendar object consists of a sequence of calendar
+ *      properties and one or more calendar components. The calendar
+ *      properties are attributes that apply to the calendar as a whole. The
+ *      calendar components are collections of properties that express a
+ *      particular calendar semantic. For example, the calendar component can
+ *      specify an event, a to-do, a journal entry, time zone information, or
+ *      free/busy time information, or an alarm.
+ *   
+ *      The body of the iCalendar object is defined by the following
+ *      notation:
+ *   
+ *        icalbody   = calprops component
+ *   
+ *        calprops   = 2*(
+ *   
+ *                   ; 'prodid' and 'version' are both REQUIRED,
+ *                   ; but MUST NOT occur more than once
+ *   
+ *                   prodid /version /
+ *   
+ *                   ; 'calscale' and 'method' are optional,
+ *                   ; but MUST NOT occur more than once
+ *   
+ *                   calscale        /
+ *                   method          /
+ *   
+ *                   x-prop
+ *   
+ *                   )
+ *   
+ *        component  = 1*(eventc / todoc / journalc / freebusyc /
+ *                   / timezonec / iana-comp / x-comp)
+ *   
+ *        iana-comp  = "BEGIN" ":" iana-token CRLF
+ *   
+ *                     1*contentline
+ *   
+ *                     "END" ":" iana-token CRLF
+ *   
+ *        x-comp     = "BEGIN" ":" x-name CRLF
+ *   
+ *                     1*contentline
+ *   
+ *                     "END" ":" x-name CRLF
+ * </pre>
+ * 
  * Example 1 - Creating a new calendar:
  * 
  * <pre><code>
