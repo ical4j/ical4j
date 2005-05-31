4b436b48efa8e4778020faa97690f63b55384b3d
fortuna <fortuna@users.sourceforge.net>
2005-05-31 13:53:26 +0000

    Implemented serialVersionUID

--- source/net/fortuna/ical4j/model/ValidationException.java
+++ source/net/fortuna/ical4j/model/ValidationException.java
@@ -38,6 +38,8 @@ package net.fortuna.ical4j.model;
  * @author benfortuna
  */
 public class ValidationException extends Exception {
+    
+    private static final long serialVersionUID = 309245291364742896L;
 
     /**
      * Default constructor.
