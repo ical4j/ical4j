fdff44d7432aaeca9b9163295a1a8f1572472c0d
fortuna <fortuna@users.sourceforge.net>
2005-06-16 09:28:38 +0000

    New test for ParameterFactory

---
+++ test/net/fortuna/ical4j/model/ParameterFactoryImplTest.java
@@ -0,0 +1,51 @@
+/*
+ * Created on 15/06/2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package net.fortuna.ical4j.model;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Ben_Fortuna
+ */
+public class ParameterFactoryImplTest extends TestCase {
+    
+    private static Log log = LogFactory.getLog(ParameterFactoryImplTest.class);
+
+	/**
+	 * @throws Exception
+	 */
+	public void testCreateParameter() throws Exception {
+        Parameter p = ParameterFactoryImpl.getInstance().createParameter(Parameter.ALTREP, "Test");
+        assertNotNull(p);
+        log.info(p);
+	}
+
+    /**
+     * @throws Exception
+     */
+    public void testCreateExperimentalParameter() throws Exception {
+        Parameter p = ParameterFactoryImpl.getInstance().createParameter("X-my-param", "Test");
+        assertNotNull(p);
+        log.info(p);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testInvalidParameter() throws Exception {
+        try {
+            Parameter p = ParameterFactoryImpl.getInstance().createParameter("my-param", "Test");
+            fail("Should throw an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae) {
+            log.info("Invalid parameter", iae);
+        }
+    }
+}
