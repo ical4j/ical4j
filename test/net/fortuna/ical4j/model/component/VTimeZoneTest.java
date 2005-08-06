86bfe3488a9d00a9f622e504dbb3459a99b44da1
fortuna <fortuna@users.sourceforge.net>
2005-08-06 06:39:37 +0000

    Added testing of VTimeZone.getTimezone()

--- test/net/fortuna/ical4j/model/component/VTimeZoneTest.java
+++ test/net/fortuna/ical4j/model/component/VTimeZoneTest.java
@@ -6,6 +6,8 @@
 package net.fortuna.ical4j.model.component;
 
 import junit.framework.TestCase;
+import net.fortuna.ical4j.model.Calendar;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -36,12 +38,33 @@ public class VTimeZoneTest extends TestCase {
      */
     public final void testGetVTimeZone() {
         TimeZone tz = TimeZone.getTimeZone("Asia/Singapore");
-        
-        VTimeZone timezone = VTimeZone.getVTimeZone(tz);
-
+        VTimeZone timezone = VTimeZone.getVTimeZone(tz.getID());
         assertNotNull(timezone);
-
         log.info(timezone);
     }
 
+    /**
+     * 
+     */
+    public void testCreateDefinition() {
+        Calendar calendar = new Calendar();
+        calendar.getComponents().add(VTimeZone.getDefault());
+        log.info(calendar);
+    }
+    
+    /**
+     * 
+     */
+    public void testGetTimeZone() {
+        VTimeZone melbourne = VTimeZone.getVTimeZone("Australia/Melbourne");
+        log.info("VTimeZone (Melbourne): " + melbourne);
+        log.info("VTimeZone (Melbourne): " + melbourne.getTimeZone());
+        
+        VTimeZone vTz = VTimeZone.getDefault();
+        log.info("VTimeZone: " + vTz.getTimeZone());
+        TimeZone tz = TimeZone.getDefault();
+        log.info("TimeZone: " + tz);
+        
+        assertTrue("Timezone rules are not the same", vTz.getTimeZone().hasSameRules(tz));
+    }
 }
