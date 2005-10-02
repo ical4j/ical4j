4ea47b134e5126befb9d09a3f80652ebc17edc48
fortuna <fortuna@users.sourceforge.net>
2005-10-02 09:04:12 +0000

    Added more tests to suite.

--- test/net/fortuna/ical4j/AllTests.java
+++ test/net/fortuna/ical4j/AllTests.java
@@ -4,9 +4,15 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import net.fortuna.ical4j.data.CalendarBuilderTest;
 import net.fortuna.ical4j.model.CalendarTest;
+import net.fortuna.ical4j.model.DateTest;
+import net.fortuna.ical4j.model.DateTimeTest;
+import net.fortuna.ical4j.model.DurTest;
+import net.fortuna.ical4j.model.NumberListTest;
+import net.fortuna.ical4j.model.ParameterFactoryImplTest;
 import net.fortuna.ical4j.model.PeriodListTest;
 import net.fortuna.ical4j.model.PeriodTest;
 import net.fortuna.ical4j.model.RecurTest;
+import net.fortuna.ical4j.model.TimeZoneTest;
 import net.fortuna.ical4j.model.WeekDayTest;
 import net.fortuna.ical4j.model.component.VEventTest;
 import net.fortuna.ical4j.model.component.VFreeBusyTest;
@@ -14,6 +20,7 @@ import net.fortuna.ical4j.model.component.VTimeZoneTest;
 import net.fortuna.ical4j.model.parameter.AltRepTest;
 import net.fortuna.ical4j.model.property.AttachTest;
 import net.fortuna.ical4j.model.property.CalScaleTest;
+import net.fortuna.ical4j.model.property.DtEndTest;
 import net.fortuna.ical4j.model.property.TriggerTest;
 import net.fortuna.ical4j.model.property.VersionTest;
 import net.fortuna.ical4j.util.StringsTest;
@@ -37,9 +44,15 @@ public class AllTests extends TestSuite{
 
         // model tests
         suite.addTestSuite(CalendarTest.class);
-        suite.addTestSuite(PeriodTest.class);
+        suite.addTestSuite(DateTest.class);
+        suite.addTestSuite(DateTimeTest.class);
+        suite.addTestSuite(DurTest.class);
+        suite.addTestSuite(NumberListTest.class);
+        suite.addTestSuite(ParameterFactoryImplTest.class);
         suite.addTestSuite(PeriodListTest.class);
+        suite.addTestSuite(PeriodTest.class);
         suite.addTestSuite(RecurTest.class);
+        suite.addTestSuite(TimeZoneTest.class);
         suite.addTestSuite(WeekDayTest.class);
 
         // component tests
@@ -53,6 +66,7 @@ public class AllTests extends TestSuite{
         // property tests
         suite.addTestSuite(AttachTest.class);
         suite.addTestSuite(CalScaleTest.class);
+        suite.addTestSuite(DtEndTest.class);
         suite.addTestSuite(TriggerTest.class);
         suite.addTestSuite(VersionTest.class);
 
