8e11a8e4ad0d32bf50eeef45f3fd221d7fc24bff
fortuna <fortuna@users.sourceforge.net>
2005-12-12 11:19:05 +0000

    Additional unit tests for properties

---
+++ test/net/fortuna/ical4j/model/property/ExDateTest.java
@@ -0,0 +1,69 @@
+/*
+ * $Id$
+ *
+ * Created on 10/12/2005
+ *
+ * Copyright (c) 2005, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.ical4j.model.property;
+
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import junit.framework.TestCase;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.PropertyList;
+
+/**
+ * Unit tests for the ExDate property.
+ * @author Ben Fortuna
+ */
+public class ExDateTest extends TestCase {
+
+    /**
+     * Ensure timezones are correctly parsed for this property.
+     * @throws Exception
+     */
+    public void testTimeZones() throws Exception {
+        CalendarBuilder builder = new CalendarBuilder();
+        Calendar calendar = builder.build(new FileInputStream("etc/samples/valid/EXDATE.ics"));
+        
+        Component event = calendar.getComponents().getComponent(Component.VEVENT);
+        PropertyList exdates = event.getProperties().getProperties(Property.EXDATE);
+        for (Iterator i = exdates.iterator(); i.hasNext();) {
+            ExDate exdate = (ExDate) i.next();
+            assertNotNull("This EXDATE should have a timezone", exdate.getDates().getTimeZone());
+        }
+    }
+}
