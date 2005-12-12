0c1b4956898eca20ce9c47f0aa9c93d34eff786e
fortuna <fortuna@users.sourceforge.net>
2005-12-12 11:17:44 +0000

    Additional unit tests for lists

---
+++ test/net/fortuna/ical4j/model/AddressListTest.java
@@ -0,0 +1,67 @@
+/*
+ * $Id$
+ *
+ * Created on 16/11/2005
+ *
+ * Copyright (c) 2005, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.ical4j.model;
+
+import junit.framework.TestCase;
+
+public class AddressListTest extends TestCase {
+
+    private static final String VALUE_ADDRESS_LIST = "\"address1@example.com\",\"address2@example.com\",\"address3@example.com\"";
+    
+    private AddressList addresses;
+    
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        addresses = new AddressList(VALUE_ADDRESS_LIST);
+    }
+
+    /**
+     * Assert three addresses parsed from value.
+     */
+    public void testSize() {
+        assertEquals(3, addresses.size());
+    }
+    
+    /**
+     * Assert toString() produces identical address list string value.
+     */
+    public void testToString() {
+        assertEquals(VALUE_ADDRESS_LIST, addresses.toString());
+    }
+}
