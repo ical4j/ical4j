f565e9d6cf847057e63c5177ce67272dda736c60
fortuna <fortuna@users.sourceforge.net>
2006-05-13 09:18:31 +0000

    Added support for codecs supplied by the Apache Commons Codec library.

---
+++ source/net/fortuna/ical4j/util/DecoderFactory.java
@@ -0,0 +1,93 @@
+/*
+ * $Id$
+ *
+ * Created on 13/05/2006
+ *
+ * Copyright (c) 2006, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.ical4j.util;
+
+import java.io.UnsupportedEncodingException;
+
+import net.fortuna.ical4j.model.parameter.Encoding;
+
+import org.apache.commons.codec.BinaryDecoder;
+import org.apache.commons.codec.StringDecoder;
+
+/**
+ * @author Ben Fortuna
+ */
+public abstract class DecoderFactory {
+    
+    /**
+     * The system property used to specify an alternate
+     * <code>DecoderFactory</code> implementation.
+     */
+    public static final String KEY_FACTORY_CLASS = "net.fortuna.ical4j.factory.decoder";
+
+    private static DecoderFactory instance;
+    static {
+        try {
+            Class factoryClass = Class.forName(System.getProperty(KEY_FACTORY_CLASS));
+            instance = (DecoderFactory) factoryClass.newInstance();
+        }
+        catch (Exception e) {
+            instance = new DefaultDecoderFactory();
+        }
+    }
+    
+    /**
+     * @return Returns the instance.
+     */
+    public static final DecoderFactory getInstance() {
+        return instance;
+    }
+
+    /**
+     * Returns a new {@link BinaryDecoder} for the specified encoding.
+     * @param encoding
+     * @return a {@link BinaryDecoder} instance
+     * @throws UnsupportedEncodingException where an encoder supporting the
+     * specified encoding is not available.
+     */
+    public abstract BinaryDecoder createBinaryDecoder(Encoding encoding)
+        throws UnsupportedEncodingException;
+    
+    /**
+     * Returns a new {@link StringDecoder} for the specified encoding.
+     * @param encoding
+     * @return a {@link StringDecoder} instance
+     * @throws UnsupportedEncodingException where an encoder supporting the
+     * specified encoding is not available.
+     */
+    public abstract StringDecoder createStringDecoder(Encoding encoding)
+        throws UnsupportedEncodingException;
+}
---
+++ source/net/fortuna/ical4j/util/DefaultEncoderFactory.java
@@ -0,0 +1,80 @@
+/*
+ * $Id$
+ *
+ * Created on 13/05/2006
+ *
+ * Copyright (c) 2006, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.ical4j.util;
+
+import java.io.UnsupportedEncodingException;
+
+import net.fortuna.ical4j.model.parameter.Encoding;
+
+import org.apache.commons.codec.BinaryEncoder;
+import org.apache.commons.codec.StringEncoder;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.net.QuotedPrintableCodec;
+
+/**
+ * @author Ben Fortuna
+ */
+public class DefaultEncoderFactory extends EncoderFactory {
+
+    /* (non-Javadoc)
+     * @see net.fortuna.ical4j.util.EncoderFactory#createBinaryEncoder(net.fortuna.ical4j.model.parameter.Encoding)
+     */
+    public BinaryEncoder createBinaryEncoder(Encoding encoding)
+            throws UnsupportedEncodingException {
+
+        if (Encoding.QUOTED_PRINTABLE.equals(encoding)) {
+            return new QuotedPrintableCodec();
+        }
+        else if (Encoding.BASE64.equals(encoding)) {
+            return new Base64();
+        }
+        throw new UnsupportedEncodingException(
+                "Encoder not available for encoding [" + encoding + "]");
+    }
+
+    /* (non-Javadoc)
+     * @see net.fortuna.ical4j.util.EncoderFactory#createStringEncoder(net.fortuna.ical4j.model.parameter.Encoding)
+     */
+    public StringEncoder createStringEncoder(Encoding encoding)
+            throws UnsupportedEncodingException {
+
+        if (Encoding.QUOTED_PRINTABLE.equals(encoding)) {
+            return new QuotedPrintableCodec();
+        }
+        throw new UnsupportedEncodingException(
+                "Encoder not available for encoding [" + encoding + "]");
+    }
+}
