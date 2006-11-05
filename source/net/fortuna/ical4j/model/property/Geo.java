08e25d3bc215f8135403611cc0767b2fafd5a595
fortuna <fortuna@users.sourceforge.net>
2006-11-05 05:20:21 +0000

    Fixed spelling mistake (lattitude)

--- source/net/fortuna/ical4j/model/property/Geo.java
+++ source/net/fortuna/ical4j/model/property/Geo.java
@@ -1,6 +1,6 @@
 /*
  * $Id$
- * 
+ *
  * Created: [Apr 6, 2004]
  *
  * Copyright (c) 2004, Ben Fortuna
@@ -24,7 +24,8 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -45,93 +46,93 @@ import net.fortuna.ical4j.model.ValidationException;
  * Defines a GEO iCalendar component property.
  * 
  * <pre>
- * 4.8.1.6 Geographic Position
- * 
- *    Property Name: GEO
- * 
- *    Purpose: This property specifies information related to the global
- *    position for the activity specified by a calendar component.
- * 
- *    Value Type: FLOAT. The value MUST be two SEMICOLON separated FLOAT
- *    values.
- * 
- *    Property Parameters: Non-standard property parameters can be
- *    specified on this property.
- * 
- *    Conformance: This property can be specified in  "VEVENT" or "VTODO"
- *    calendar components.
- * 
- *    Description: The property value specifies latitude and longitude, in
- *    that order (i.e., "LAT LON" ordering). The longitude represents the
- *    location east or west of the prime meridian as a positive or negative
- *    real number, respectively. The longitude and latitude values MAY be
- *    specified up to six decimal places, which will allow for accuracy to
- *    within one meter of geographical position. Receiving applications
- *    MUST accept values of this precision and MAY truncate values of
- *    greater precision.
- * 
- *    Values for latitude and longitude shall be expressed as decimal
- *    fractions of degrees. Whole degrees of latitude shall be represented
- *    by a two-digit decimal number ranging from 0 through 90. Whole
- *    degrees of longitude shall be represented by a decimal number ranging
- *    from 0 through 180. When a decimal fraction of a degree is specified,
- *    it shall be separated from the whole number of degrees by a decimal
- *    point.
- * 
- *    Latitudes north of the equator shall be specified by a plus sign (+),
- *    or by the absence of a minus sign (-), preceding the digits
- *    designating degrees. Latitudes south of the Equator shall be
- *    designated by a minus sign (-) preceding the digits designating
- *    degrees. A point on the Equator shall be assigned to the Northern
- *    Hemisphere.
- * 
- *    Longitudes east of the prime meridian shall be specified by a plus
- *    sign (+), or by the absence of a minus sign (-), preceding the digits
- *    designating degrees. Longitudes west of the meridian shall be
- *    designated by minus sign (-) preceding the digits designating
- *    degrees. A point on the prime meridian shall be assigned to the
- *    Eastern Hemisphere. A point on the 180th meridian shall be assigned
- *    to the Western Hemisphere. One exception to this last convention is
- *    permitted. For the special condition of describing a band of latitude
- *    around the earth, the East Bounding Coordinate data element shall be
- *    assigned the value +180 (180) degrees.
- * 
- *    Any spatial address with a latitude of +90 (90) or -90 degrees will
- *    specify the position at the North or South Pole, respectively. The
- *    component for longitude may have any legal value.
- * 
- *    With the exception of the special condition described above, this
- *    form is specified in Department of Commerce, 1986, Representation of
- *    geographic point locations for information interchange (Federal
- *    Information Processing Standard 70-1):  Washington,  Department of
- *    Commerce, National Institute of Standards and Technology.
- * 
- *    The simple formula for converting degrees-minutes-seconds into
- *    decimal degrees is:
- * 
- *      decimal = degrees + minutes/60 + seconds/3600.
- * 
- *    Format Definition: The property is defined by the following notation:
- * 
- *      geo        = "GEO" geoparam ":" geovalue CRLF
- * 
- *      geoparam   = *(";" xparam)
- * 
- *      geovalue   = float ";" float
- *      ;Latitude and Longitude components
- * 
- *    Example: The following is an example of this property:
- * 
- *      GEO:37.386013;-122.082932
+ *  4.8.1.6 Geographic Position
+ *  
+ *     Property Name: GEO
+ *  
+ *     Purpose: This property specifies information related to the global
+ *     position for the activity specified by a calendar component.
+ *  
+ *     Value Type: FLOAT. The value MUST be two SEMICOLON separated FLOAT
+ *     values.
+ *  
+ *     Property Parameters: Non-standard property parameters can be
+ *     specified on this property.
+ *  
+ *     Conformance: This property can be specified in  &quot;VEVENT&quot; or &quot;VTODO&quot;
+ *     calendar components.
+ *  
+ *     Description: The property value specifies latitude and longitude, in
+ *     that order (i.e., &quot;LAT LON&quot; ordering). The longitude represents the
+ *     location east or west of the prime meridian as a positive or negative
+ *     real number, respectively. The longitude and latitude values MAY be
+ *     specified up to six decimal places, which will allow for accuracy to
+ *     within one meter of geographical position. Receiving applications
+ *     MUST accept values of this precision and MAY truncate values of
+ *     greater precision.
+ *  
+ *     Values for latitude and longitude shall be expressed as decimal
+ *     fractions of degrees. Whole degrees of latitude shall be represented
+ *     by a two-digit decimal number ranging from 0 through 90. Whole
+ *     degrees of longitude shall be represented by a decimal number ranging
+ *     from 0 through 180. When a decimal fraction of a degree is specified,
+ *     it shall be separated from the whole number of degrees by a decimal
+ *     point.
+ *  
+ *     Latitudes north of the equator shall be specified by a plus sign (+),
+ *     or by the absence of a minus sign (-), preceding the digits
+ *     designating degrees. Latitudes south of the Equator shall be
+ *     designated by a minus sign (-) preceding the digits designating
+ *     degrees. A point on the Equator shall be assigned to the Northern
+ *     Hemisphere.
+ *  
+ *     Longitudes east of the prime meridian shall be specified by a plus
+ *     sign (+), or by the absence of a minus sign (-), preceding the digits
+ *     designating degrees. Longitudes west of the meridian shall be
+ *     designated by minus sign (-) preceding the digits designating
+ *     degrees. A point on the prime meridian shall be assigned to the
+ *     Eastern Hemisphere. A point on the 180th meridian shall be assigned
+ *     to the Western Hemisphere. One exception to this last convention is
+ *     permitted. For the special condition of describing a band of latitude
+ *     around the earth, the East Bounding Coordinate data element shall be
+ *     assigned the value +180 (180) degrees.
+ *  
+ *     Any spatial address with a latitude of +90 (90) or -90 degrees will
+ *     specify the position at the North or South Pole, respectively. The
+ *     component for longitude may have any legal value.
+ *  
+ *     With the exception of the special condition described above, this
+ *     form is specified in Department of Commerce, 1986, Representation of
+ *     geographic point locations for information interchange (Federal
+ *     Information Processing Standard 70-1):  Washington,  Department of
+ *     Commerce, National Institute of Standards and Technology.
+ *  
+ *     The simple formula for converting degrees-minutes-seconds into
+ *     decimal degrees is:
+ *  
+ *       decimal = degrees + minutes/60 + seconds/3600.
+ *  
+ *     Format Definition: The property is defined by the following notation:
+ *  
+ *       geo        = &quot;GEO&quot; geoparam &quot;:&quot; geovalue CRLF
+ *  
+ *       geoparam   = *(&quot;;&quot; xparam)
+ *  
+ *       geovalue   = float &quot;;&quot; float
+ *       ;Latitude and Longitude components
+ *  
+ *     Example: The following is an example of this property:
+ *  
+ *       GEO:37.386013;-122.082932
  * </pre>
  * 
  * @author Ben Fortuna
  */
 public class Geo extends Property {
-    
+
     private static final long serialVersionUID = -902100715801867636L;
 
-    private BigDecimal lattitude;
+    private BigDecimal latitude;
 
     private BigDecimal longitude;
 
@@ -140,10 +141,10 @@ public class Geo extends Property {
      */
     public Geo() {
         super(GEO);
-        lattitude = new BigDecimal(0);
+        latitude = new BigDecimal(0);
         longitude = new BigDecimal(0);
     }
-    
+
     /**
      * Creates a new instance by parsing the specified string representation.
      * @param value
@@ -152,12 +153,10 @@ public class Geo extends Property {
         super(GEO);
         setValue(value);
     }
-    
+
     /**
-     * @param aList
-     *            a list of parameters for this component
-     * @param aValue
-     *            a value string for this component
+     * @param aList a list of parameters for this component
+     * @param aValue a value string for this component
      */
     public Geo(final ParameterList aList, final String aValue) {
         super(GEO, aList);
@@ -165,37 +164,32 @@ public class Geo extends Property {
     }
 
     /**
-     * @param aLattitude
-     *            a lattitudinal value
-     * @param aLongitude
-     *            a longitudinal value
+     * @param latitude a latitudinal value
+     * @param longitude a longitudinal value
      */
-    public Geo(final BigDecimal aLattitude, final BigDecimal aLongitude) {
+    public Geo(final BigDecimal latitude, final BigDecimal longitude) {
         super(GEO);
-        lattitude = aLattitude;
-        longitude = aLongitude;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     /**
-     * @param aList
-     *            a list of parameters for this component
-     * @param aLattitude
-     *            a lattitudinal value
-     * @param aLongitude
-     *            a longitudinal value
+     * @param aList a list of parameters for this component
+     * @param latitude a latitudinal value
+     * @param longitude a longitudinal value
      */
-    public Geo(final ParameterList aList, final BigDecimal aLattitude,
-            final BigDecimal aLongitude) {
+    public Geo(final ParameterList aList, final BigDecimal latitude,
+            final BigDecimal longitude) {
         super(GEO, aList);
-        lattitude = aLattitude;
-        longitude = aLongitude;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     /**
-     * @return Returns the lattitude.
+     * @return Returns the latitude.
      */
-    public final BigDecimal getLattitude() {
-        return lattitude;
+    public final BigDecimal getLatitude() {
+        return latitude;
     }
 
     /**
@@ -204,41 +198,41 @@ public class Geo extends Property {
     public final BigDecimal getLongitude() {
         return longitude;
     }
-    
-    
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
      * @see net.fortuna.ical4j.model.Property#setValue(java.lang.String)
      */
     public final void setValue(final String aValue) {
-        lattitude = new BigDecimal(aValue.substring(0, aValue.indexOf(';')));
-        longitude =  new BigDecimal(aValue.substring(aValue.indexOf(';') + 1));
+        latitude = new BigDecimal(aValue.substring(0, aValue.indexOf(';')));
+        longitude = new BigDecimal(aValue.substring(aValue.indexOf(';') + 1));
     }
 
     /*
      * (non-Javadoc)
-     *
      * @see net.fortuna.ical4j.model.Property#getValue()
      */
     public final String getValue() {
-        return String.valueOf(getLattitude()) + ";"
+        return String.valueOf(getLatitude()) + ";"
                 + String.valueOf(getLongitude());
     }
-    
+
     /**
-     * @param lattitude The lattitude to set.
+     * @param latitude The latitude to set.
      */
-    public final void setLattitude(final BigDecimal lattitude) {
-        this.lattitude = lattitude;
+    public final void setLatitude(final BigDecimal latitude) {
+        this.latitude = latitude;
     }
-    
+
     /**
      * @param longitude The longitude to set.
      */
     public final void setLongitude(final BigDecimal longitude) {
         this.longitude = longitude;
     }
-    
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
      * @see net.fortuna.ical4j.model.Property#validate()
      */
     public final void validate() throws ValidationException {
