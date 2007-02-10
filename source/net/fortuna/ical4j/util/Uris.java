069097f51d9c3749f31a2cdd700d2c34cbdab597
fortuna <fortuna@users.sourceforge.net>
2007-02-10 08:32:21 +0000

    Added convenience method for creating URIs that will swallow any exceptions (and return null) if relaxed parsing is enabled.

--- source/net/fortuna/ical4j/util/Uris.java
+++ source/net/fortuna/ical4j/util/Uris.java
@@ -24,7 +24,7 @@
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -35,6 +35,8 @@
  */
 package net.fortuna.ical4j.util;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Utility methods for working with URIs.
@@ -42,10 +44,10 @@ package net.fortuna.ical4j.util;
  */
 public final class Uris {
 
-//    private static final String ENCODING_CHARSET = "UTF-8";
-    
-//    private static Log log = LogFactory.getLog(Uris.class);
-    
+    // private static final String ENCODING_CHARSET = "UTF-8";
+
+    // private static Log log = LogFactory.getLog(Uris.class);
+
     /**
      * Constructor made private to enforce static nature.
      */
@@ -53,62 +55,65 @@ public final class Uris {
     }
 
     /**
-     * Encodes the specified URI string using the UTF-8 charset. In the
-     * event that an exception is thrown, the specifed URI string is
-     * returned unmodified.
+     * Encodes the specified URI string using the UTF-8 charset. In the event that an exception is thrown, the specifed
+     * URI string is returned unmodified.
      * @param s a URI string
      * @return an encoded URI string
      */
     public static String encode(final String s) {
         /*
-        try {
-            return URLEncoder.encode(s, ENCODING_CHARSET);
-        }
-        catch (UnsupportedEncodingException use) {
-            log.error("Error ocurred encoding URI [" + s + "]", use);
-        }
-        */
-        
-        /*
-         * Lotus Notes does not correctly strip angle brackets from cid uris.
-         * From RFC2932:
-         * 
-         * A "cid" URL is converted to the corresponding Content-ID message
-         * header [MIME] by removing the "cid:" prefix, converting the % encoded
-         * character to their equivalent US-ASCII characters, and enclosing the
-         * remaining parts with an angle bracket pair, "<" and ">".  For
-         * example, "cid:foo4%25foo1@bar.net" corresponds to
-         * 
-         * Content-ID: <foo4%25foo1@bar.net>
-         * 
-         * Reversing the process and converting URL special characters to their
-         * % encodings produces the original cid.
-         * 
-         * A "mid" URL is converted to a Message-ID or Message-ID/Content-ID
-         * pair in a similar fashion.
+         * try { return URLEncoder.encode(s, ENCODING_CHARSET); } catch (UnsupportedEncodingException use) {
+         * log.error("Error ocurred encoding URI [" + s + "]", use); }
          */
-        if (CompatibilityHints.isHintEnabled(CompatibilityHints.KEY_NOTES_COMPATIBILITY)) {
+
+        /*
+         * Lotus Notes does not correctly strip angle brackets from cid uris. From RFC2932: A "cid" URL is converted to
+         * the corresponding Content-ID message header [MIME] by removing the "cid:" prefix, converting the % encoded
+         * character to their equivalent US-ASCII characters, and enclosing the remaining parts with an angle bracket
+         * pair, "<" and ">". For example, "cid:foo4%25foo1@bar.net" corresponds to Content-ID: <foo4%25foo1@bar.net>
+         * Reversing the process and converting URL special characters to their % encodings produces the original cid. A
+         * "mid" URL is converted to a Message-ID or Message-ID/Content-ID pair in a similar fashion.
+         */
+        if (CompatibilityHints
+                .isHintEnabled(CompatibilityHints.KEY_NOTES_COMPATIBILITY)) {
             return s.replaceAll("[<>]", "");
         }
         return s;
     }
-    
+
     /**
-     * Decodes the specified URI string using the UTF-8 charset. In the
-     * event that an exception is thrown, the specifed URI string is
-     * returned unmodified.
+     * Decodes the specified URI string using the UTF-8 charset. In the event that an exception is thrown, the specifed
+     * URI string is returned unmodified.
      * @param s a URI string
      * @return an encoded URI string
      */
     public static String decode(final String s) {
         /*
-        try {
-            return URLDecoder.decode(s, ENCODING_CHARSET);
-        }
-        catch (UnsupportedEncodingException use) {
-            log.error("Error ocurred decoding URI [" + s + "]", use);
-        }
-        */
+         * try { return URLDecoder.decode(s, ENCODING_CHARSET); } catch (UnsupportedEncodingException use) {
+         * log.error("Error ocurred decoding URI [" + s + "]", use); }
+         */
         return s;
+    }
+
+    /**
+     * Attempts to create a URI instance and will optionally swallow any resulting URISyntaxException depending on
+     * configured {@link CompatibilityHints}. Will also automatically attempt encoding of the string representation for
+     * greater compatibility.
+     * @param s a string representation of a URI.
+     * @return a URI instance, or null if a valid URI string is not specified and relaxed parsing is enabled.
+     * @throws URISyntaxException if a valid URI string is not specified and relaxed parsing is disabled
+     */
+    public static URI create(final String s) throws URISyntaxException {
+        try {
+            return new URI(encode(s));
+        }
+        catch (URISyntaxException use) {
+            if (CompatibilityHints
+                    .isHintEnabled(CompatibilityHints.KEY_RELAXED_PARSING)) {
+
+                return null;
+            }
+            throw use;
+        }
     }
 }
