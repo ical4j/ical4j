4dab7c8ad8eff4081aca33ac5e002c38d3f6bab9
fortuna <fortuna@users.sourceforge.net>
2007-03-11 05:06:32 +0000

    Added copy constructor for creating a deep copy of a calendar

--- source/net/fortuna/ical4j/model/Calendar.java
+++ source/net/fortuna/ical4j/model/Calendar.java
@@ -33,11 +33,11 @@
  */
 package net.fortuna.ical4j.model;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.URISyntaxException;
+import java.text.ParseException;
 import java.util.Iterator;
-
-import org.apache.commons.lang.ObjectUtils;
-import org.apache.commons.lang.builder.HashCodeBuilder;
 
 import net.fortuna.ical4j.model.component.CalendarComponent;
 import net.fortuna.ical4j.model.property.CalScale;
@@ -48,73 +48,76 @@ import net.fortuna.ical4j.model.property.XProperty;
 import net.fortuna.ical4j.util.PropertyValidator;
 import net.fortuna.ical4j.util.Strings;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 /**
  * Defines an iCalendar calendar.
  * 
  * <pre>
- *   4.6 Calendar Components
- *   
- *      The body of the iCalendar object consists of a sequence of calendar
- *      properties and one or more calendar components. The calendar
- *      properties are attributes that apply to the calendar as a whole. The
- *      calendar components are collections of properties that express a
- *      particular calendar semantic. For example, the calendar component can
- *      specify an event, a to-do, a journal entry, time zone information, or
- *      free/busy time information, or an alarm.
- *   
- *      The body of the iCalendar object is defined by the following
- *      notation:
- *   
- *        icalbody   = calprops component
- *   
- *        calprops   = 2*(
- *   
- *                   ; 'prodid' and 'version' are both REQUIRED,
- *                   ; but MUST NOT occur more than once
- *   
- *                   prodid /version /
- *   
- *                   ; 'calscale' and 'method' are optional,
- *                   ; but MUST NOT occur more than once
- *   
- *                   calscale        /
- *                   method          /
- *   
- *                   x-prop
- *   
- *                   )
- *   
- *        component  = 1*(eventc / todoc / journalc / freebusyc /
- *                   / timezonec / iana-comp / x-comp)
- *   
- *        iana-comp  = "BEGIN" ":" iana-token CRLF
- *   
- *                     1*contentline
- *   
- *                     "END" ":" iana-token CRLF
- *   
- *        x-comp     = "BEGIN" ":" x-name CRLF
- *   
- *                     1*contentline
- *   
- *                     "END" ":" x-name CRLF
+ *    4.6 Calendar Components
+ *    
+ *       The body of the iCalendar object consists of a sequence of calendar
+ *       properties and one or more calendar components. The calendar
+ *       properties are attributes that apply to the calendar as a whole. The
+ *       calendar components are collections of properties that express a
+ *       particular calendar semantic. For example, the calendar component can
+ *       specify an event, a to-do, a journal entry, time zone information, or
+ *       free/busy time information, or an alarm.
+ *    
+ *       The body of the iCalendar object is defined by the following
+ *       notation:
+ *    
+ *         icalbody   = calprops component
+ *    
+ *         calprops   = 2*(
+ *    
+ *                    ; 'prodid' and 'version' are both REQUIRED,
+ *                    ; but MUST NOT occur more than once
+ *    
+ *                    prodid /version /
+ *    
+ *                    ; 'calscale' and 'method' are optional,
+ *                    ; but MUST NOT occur more than once
+ *    
+ *                    calscale        /
+ *                    method          /
+ *    
+ *                    x-prop
+ *    
+ *                    )
+ *    
+ *         component  = 1*(eventc / todoc / journalc / freebusyc /
+ *                    / timezonec / iana-comp / x-comp)
+ *    
+ *         iana-comp  = &quot;BEGIN&quot; &quot;:&quot; iana-token CRLF
+ *    
+ *                      1*contentline
+ *    
+ *                      &quot;END&quot; &quot;:&quot; iana-token CRLF
+ *    
+ *         x-comp     = &quot;BEGIN&quot; &quot;:&quot; x-name CRLF
+ *    
+ *                      1*contentline
+ *    
+ *                      &quot;END&quot; &quot;:&quot; x-name CRLF
  * </pre>
  * 
  * Example 1 - Creating a new calendar:
  * 
  * <pre><code>
  * Calendar calendar = new Calendar();
- * calendar.getProperties().add(new ProdId("-//Ben Fortuna//iCal4j 1.0//EN"));
+ * calendar.getProperties().add(new ProdId(&quot;-//Ben Fortuna//iCal4j 1.0//EN&quot;));
  * calendar.getProperties().add(Version.VERSION_2_0);
  * calendar.getProperties().add(CalScale.GREGORIAN);
- *
+ * 
  * // Add events, etc..
  * </code></pre>
- *
+ * 
  * @author Ben Fortuna
  */
 public class Calendar implements Serializable {
-    
+
     private static final long serialVersionUID = -1654118204678581940L;
 
     public static final String BEGIN = "BEGIN";
@@ -135,10 +138,8 @@ public class Calendar implements Serializable {
     }
 
     /**
-     * Constructs a new calendar with no properties and
-     * the specified components.
-     * @param components a list of components to add to
-     * the calendar
+     * Constructs a new calendar with no properties and the specified components.
+     * @param components a list of components to add to the calendar
      */
     public Calendar(final ComponentList components) {
         this(new PropertyList(), components);
@@ -146,15 +147,23 @@ public class Calendar implements Serializable {
 
     /**
      * Constructor.
-     *
-     * @param p
-     *            a list of properties
-     * @param c
-     *            a list of components
+     * @param p a list of properties
+     * @param c a list of components
      */
     public Calendar(final PropertyList p, final ComponentList c) {
         this.properties = p;
         this.components = c;
+    }
+
+    /**
+     * Creates a deep copy of the specified calendar.
+     * @param c the calendar to copy
+     */
+    public Calendar(Calendar c) throws ParseException, IOException,
+            URISyntaxException {
+        
+        this(new PropertyList(c.getProperties()), new ComponentList(c
+                .getComponents()));
     }
 
     /**
@@ -182,22 +191,20 @@ public class Calendar implements Serializable {
     public final ComponentList getComponents() {
         return components;
     }
-    
+
     /**
      * Convenience method for retrieving a list of named components.
      * @param name name of components to retrieve
-     * @return a component list containing only components with the specified
-     * name
+     * @return a component list containing only components with the specified name
      */
     public final ComponentList getComponents(final String name) {
         return getComponents().getComponents(name);
     }
-    
+
     /**
      * Convenience method for retrieving a named component.
      * @param name name of the component to retrieve
-     * @return the first matching component in the component list with the specified
-     * name
+     * @return the first matching component in the component list with the specified name
      */
     public final Component getComponent(final String name) {
         return getComponents().getComponent(name);
@@ -209,32 +216,28 @@ public class Calendar implements Serializable {
     public final PropertyList getProperties() {
         return properties;
     }
-    
+
     /**
      * Convenience method for retrieving a list of named properties.
      * @param name name of properties to retrieve
-     * @return a property list containing only properties with the specified
-     * name
+     * @return a property list containing only properties with the specified name
      */
     public final PropertyList getProperties(final String name) {
         return getProperties().getProperties(name);
     }
-    
+
     /**
      * Convenience method for retrieving a named property.
      * @param name name of the property to retrieve
-     * @return the first matching property in the property list with the specified
-     * name
+     * @return the first matching property in the property list with the specified name
      */
     public final Property getProperty(final String name) {
         return getProperties().getProperty(name);
     }
 
     /**
-     * Perform validation on the calendar, its properties
-     * and its components in its current state.
-     * @throws ValidationException
-     *             where the calendar is not in a valid state
+     * Perform validation on the calendar, its properties and its components in its current state.
+     * @throws ValidationException where the calendar is not in a valid state
      */
     public final void validate() throws ValidationException {
         validate(true);
@@ -242,10 +245,8 @@ public class Calendar implements Serializable {
 
     /**
      * Perform validation on the calendar in its current state.
-     * @param recurse indicates whether to validate the calendar's
-     * properties and components
-     * @throws ValidationException
-     *             where the calendar is not in a valid state
+     * @param recurse indicates whether to validate the calendar's properties and components
+     * @throws ValidationException where the calendar is not in a valid state
      */
     public void validate(final boolean recurse) throws ValidationException {
         // 'prodid' and 'version' are both REQUIRED,
@@ -255,22 +256,25 @@ public class Calendar implements Serializable {
 
         // 'calscale' and 'method' are optional,
         // but MUST NOT occur more than once
-        PropertyValidator.getInstance()
-                .assertOneOrLess(Property.CALSCALE, properties);
-        PropertyValidator.getInstance().assertOneOrLess(Property.METHOD, properties);
+        PropertyValidator.getInstance().assertOneOrLess(Property.CALSCALE,
+                properties);
+        PropertyValidator.getInstance().assertOneOrLess(Property.METHOD,
+                properties);
 
         // must contain at least one component
-        if (getComponents().isEmpty()) { throw new ValidationException(
-                "Calendar must contain at least one component"); }
+        if (getComponents().isEmpty()) {
+            throw new ValidationException(
+                    "Calendar must contain at least one component");
+        }
 
         // validate properties..
         for (Iterator i = getProperties().iterator(); i.hasNext();) {
             Property property = (Property) i.next();
 
             if (!(property instanceof XProperty)
-                 && !property.isCalendarProperty()) {
-                throw new ValidationException(
-                    "Invalid property: " + property.getName());
+                    && !property.isCalendarProperty()) {
+                throw new ValidationException("Invalid property: "
+                        + property.getName());
             }
         }
 
@@ -280,7 +284,8 @@ public class Calendar implements Serializable {
 
             if (!(component instanceof CalendarComponent)) {
                 throw new ValidationException(
-                    "Not a valid calendar component: " + component.getName());
+                        "Not a valid calendar component: "
+                                + component.getName());
             }
         }
 
@@ -292,8 +297,7 @@ public class Calendar implements Serializable {
 
     /**
      * Invoke validation on the calendar properties in its current state.
-     * @throws ValidationException
-     *             where any of the calendar properties is not in a valid state
+     * @throws ValidationException where any of the calendar properties is not in a valid state
      */
     private void validateProperties() throws ValidationException {
         for (Iterator i = getProperties().iterator(); i.hasNext();) {
@@ -304,8 +308,7 @@ public class Calendar implements Serializable {
 
     /**
      * Invoke validation on the calendar components in its current state.
-     * @throws ValidationException
-     *             where any of the calendar components is not in a valid state
+     * @throws ValidationException where any of the calendar components is not in a valid state
      */
     private void validateComponents() throws ValidationException {
         for (Iterator i = getComponents().iterator(); i.hasNext();) {
@@ -313,7 +316,7 @@ public class Calendar implements Serializable {
             component.validate();
         }
     }
-    
+
     /**
      * Returns the mandatory prodid property.
      * @return
@@ -321,7 +324,7 @@ public class Calendar implements Serializable {
     public final ProdId getProductId() {
         return (ProdId) getProperty(Property.PRODID);
     }
-    
+
     /**
      * Returns the mandatory version property.
      * @return
@@ -329,7 +332,7 @@ public class Calendar implements Serializable {
     public final Version getVersion() {
         return (Version) getProperty(Property.VERSION);
     }
-    
+
     /**
      * Returns the optional calscale property.
      * @return
@@ -337,7 +340,7 @@ public class Calendar implements Serializable {
     public final CalScale getCalendarScale() {
         return (CalScale) getProperty(Property.CALSCALE);
     }
-    
+
     /**
      * Returns the optional method property.
      * @return
@@ -345,17 +348,18 @@ public class Calendar implements Serializable {
     public final Method getMethod() {
         return (Method) getProperty(Property.METHOD);
     }
-    
+
     /**
-     * Uses {@link ObjectUtils} to test equality.
-     * Two calendars are equal if and only if their
-     * property lists and component lists are equal.
+     * Uses {@link ObjectUtils} to test equality. Two calendars are equal if and only if their property lists and
+     * component lists are equal.
      */
     public final boolean equals(final Object arg0) {
         if (arg0 instanceof Calendar) {
             Calendar calendar = (Calendar) arg0;
-            return ObjectUtils.equals(getProperties(), calendar.getProperties())
-                    && ObjectUtils.equals(getComponents(), calendar.getComponents());
+            return ObjectUtils
+                    .equals(getProperties(), calendar.getProperties())
+                    && ObjectUtils.equals(getComponents(), calendar
+                            .getComponents());
         }
         return super.equals(arg0);
     }
@@ -364,7 +368,7 @@ public class Calendar implements Serializable {
      * Uses {@link HashCodeBuilder} to build hashcode.
      */
     public final int hashCode() {
-        return new HashCodeBuilder().append(getProperties())
-            .append(getComponents()).hashCode();
+        return new HashCodeBuilder().append(getProperties()).append(
+                getComponents()).hashCode();
     }
 }
