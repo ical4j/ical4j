1d312820788ae65a88f85ae81fc470481d58e6d7
fortuna <fortuna@users.sourceforge.net>
2007-03-31 04:59:07 +0000

    Added unit tests specific to DateProperty and subclasses

---
+++ test/net/fortuna/ical4j/model/property/DatePropertyTest.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ *
+ * Created on 31/03/2007
+ *
+ * Copyright (c) 2007, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.ical4j.model.property;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+
+import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.DefaultTimeZoneRegistryFactory;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.TimeZoneRegistry;
+import junit.framework.TestCase;
+
+/**
+ * Unit tests specific to {@link DateProperty} and its subclasses.
+ * @author Ben
+ *
+ */
+public class DatePropertyTest extends TestCase {
+
+    private TimeZoneRegistry tzReg = DefaultTimeZoneRegistryFactory.getInstance().createRegistry();
+
+    /**
+     * 
+     */
+    public void testCopy() throws IOException, URISyntaxException, ParseException {
+        
+        DtStamp dtStamp = new DtStamp();
+//        dtStamp.getParameters().add(new TzId("Australia/Melbourne"));
+//        dtStamp.setTimeZone(tzReg.getTimeZone("Australia/Melbourne"));
+        
+        Property copy = dtStamp.copy();
+        assertEquals(dtStamp, copy);
+        
+        DtStart dtStart = new DtStart(new DateTime());
+//        dtStart.getParameters().add(new TzId("Australia/Melbourne"));
+        dtStart.setTimeZone(tzReg.getTimeZone("Australia/Melbourne"));
+        
+        copy = dtStart.copy();
+        assertEquals(dtStart, copy);
+        assertEquals(dtStart.timezone, ((DateProperty) copy).timezone);
+    }
+}
