8b6e8d48d523de1bac470f781f2ff4e634a2847b
fortuna <fortuna@users.sourceforge.net>
2007-04-23 13:59:34 +0000

    Revert move of date initialisation as it broke unit tests

--- source/net/fortuna/ical4j/model/property/DateProperty.java
+++ source/net/fortuna/ical4j/model/property/DateProperty.java
@@ -104,7 +104,6 @@ public abstract class DateProperty extends Property {
      * @param date The date to set.
      */
     public final void setDate(final Date date) {
-        this.date = date;
         if (date instanceof DateTime) {
             updateTimeZone(((DateTime) date).getTimeZone());
         }
@@ -112,6 +111,7 @@ public abstract class DateProperty extends Property {
             // ensure timezone is null for VALUE=DATE properties..
             updateTimeZone(null);
         }
+        this.date = date;
     }
 
     /**
