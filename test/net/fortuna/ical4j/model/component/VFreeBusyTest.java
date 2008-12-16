3f518b1d3974858933e1fc36983bf5f2482e2185
fortuna <fortuna@users.sourceforge.net>
2008-12-16 12:34:54 +0000

    Updated licence headers

--- test/net/fortuna/ical4j/model/component/VFreeBusyTest.java
+++ test/net/fortuna/ical4j/model/component/VFreeBusyTest.java
@@ -1,4 +1,7 @@
-/*
+/**
+ * Copyright (c) 2008, Ben Fortuna
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
