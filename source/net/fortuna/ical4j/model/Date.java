dc53f5ef0e73da586cb4a4ec54ef2715ba1ebe12
fortuna <fortuna@users.sourceforge.net>
2009-02-28 04:37:25 +0000

    Added specification from RFC2445

--- source/net/fortuna/ical4j/model/Date.java
+++ source/net/fortuna/ical4j/model/Date.java
@@ -45,6 +45,42 @@ import net.fortuna.ical4j.util.TimeZones;
  *
  * Base class for all representations of time values in RFC2445.
  *
+ * <pre>
+ * 4.3.4 Date
+ * 
+ *    Value Name: DATE
+ * 
+ *    Purpose: This value type is used to identify values that contain a
+ *    calendar date.
+ * 
+ *    Formal Definition: The value type is defined by the following
+ *    notation:
+ * 
+ *      date               = date-value
+ * 
+ *      date-value         = date-fullyear date-month date-mday
+ *      date-fullyear      = 4DIGIT
+ *      date-month         = 2DIGIT        ;01-12
+ *      date-mday          = 2DIGIT        ;01-28, 01-29, 01-30, 01-31
+ *                                         ;based on month/year
+ * 
+ *    Description: If the property permits, multiple "date" values are
+ *    specified as a COMMA character (US-ASCII decimal 44) separated list
+ *    of values. The format for the value type is expressed as the [ISO
+ *    8601] complete representation, basic format for a calendar date. The
+ *    textual format specifies a four-digit year, two-digit month, and
+ *    two-digit day of the month. There are no separator characters between
+ *    the year, month and day component text.
+ * 
+ *    No additional content value encoding (i.e., BACKSLASH character
+ *    encoding) is defined for this value type.
+ * 
+ *    Example: The following represents July 14, 1997:
+ * 
+ *      19970714
+ * 
+ * </pre>
+ * 
  * @author Ben Fortuna
  */
 public class Date extends Iso8601 {
