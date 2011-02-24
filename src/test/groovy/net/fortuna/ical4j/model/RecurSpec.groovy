ede209d053c0cadad1d10a2b6839c911e430c34b
fortuna <fortuna@users.sourceforge.net>
2011-02-24 11:03:19 +0000

    Added specification for Recur

---
+++ src/test/groovy/net/fortuna/ical4j/model/RecurSpec.groovy
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2011, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.ical4j.model
+
+import net.fortuna.ical4j.model.parameter.Value;
+import spock.lang.Specification;
+
+class RecurSpec extends Specification {
+
+	def 'verify recurrence rules for date-time'() {
+		expect:
+		recur.getDates(start, end, Value.DATE_TIME) == expectedDates
+		
+		where:
+		recur								| start					| end					| expectedDates
+		new Recur('FREQ=WEEKLY;BYDAY=MO')	| new Date('20110101')	| new Date('20110201')	| [new DateTime('20110103T110000'), new DateTime('20110110T110000'), new DateTime('20110117T110000'), new DateTime('20110124T110000'), new DateTime('20110131T110000')]
+	}
+}
