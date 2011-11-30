4d2bcf008f53811a9b7d262007913206613c8bd4
arnaudq <arnaudq@quillaud.org>
2011-11-30 18:07:31 +0100

    Overwrite default TimeZone.getOffset(long) as it is taking into account only the latest offset (does not work for historical times)

--- src/test/java/net/fortuna/ical4j/model/TimeZoneTest.java
+++ src/test/java/net/fortuna/ical4j/model/TimeZoneTest.java
@@ -31,12 +31,15 @@
  */
 package net.fortuna.ical4j.model;
 
+import java.io.StringReader;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.model.component.VTimeZone;
 import net.fortuna.ical4j.model.property.DtStart;
 
 import org.apache.commons.logging.Log;
@@ -314,12 +317,68 @@ public class TimeZoneTest extends TestCase {
         }
         return super.getName();
     }
+
+    public void testMinsk() throws Exception {
+        String fixedDefinition =
+        "BEGIN:VCALENDAR\r\n"
+        + "BEGIN:VTIMEZONE\r\n"
+        + "TZID:Europe/Minsk\r\n"
+        + "X-S1CS-TZID-ALIAS:E. Europe Standard Time\r\n"
+        + "BEGIN:STANDARD\r\n"
+        + "TZOFFSETFROM:+0200\r\n"
+        + "TZOFFSETTO:+0300\r\n"
+        + "TZNAME:FET\r\n"
+        + "DTSTART:20110327T020000\r\n"
+        + "RDATE:20110327T020000\r\n"
+        + "END:STANDARD\r\n"
+        + "BEGIN:DAYLIGHT\r\n"
+        + "TZOFFSETFROM:+0200\r\n"
+        + "TZOFFSETTO:+0300\r\n"
+        + "TZNAME:EEST\r\n"
+        + "DTSTART:19920329T000000\r\n"
+        + "RDATE:20020331T020000\r\n"
+        + "RDATE:20030330T020000\r\n"
+        + "RDATE:20040328T020000\r\n"
+        + "RDATE:20050327T020000\r\n"
+        + "RDATE:20060326T020000\r\n"
+        + "RDATE:20070325T020000\r\n"
+        + "RDATE:20080330T020000\r\n"
+        + "RDATE:20090329T020000\r\n"
+        + "RDATE:20100328T020000\r\n"
+        + "END:DAYLIGHT\r\n"
+        + "BEGIN:STANDARD\r\n"
+        + "TZOFFSETFROM:+0300\r\n"
+        + "TZOFFSETTO:+0200\r\n"
+        + "TZNAME:EET\r\n"
+        + "DTSTART:19910929T030000\r\n"
+        + "RDATE:20021027T030000\r\n"
+        + "RDATE:20031026T030000\r\n"
+        + "RDATE:20041031T030000\r\n"
+        + "RDATE:20051030T030000\r\n"
+        + "RDATE:20061029T030000\r\n"
+        + "RDATE:20071028T030000\r\n"
+        + "RDATE:20081026T030000\r\n"
+        + "RDATE:20091025T030000\r\n"
+        + "RDATE:20101031T030000\r\n"
+        + "END:STANDARD\r\n"
+        + "END:VTIMEZONE\r\n"
+        + "END:VCALENDAR\r\n";
+
+        net.fortuna.ical4j.model.Calendar cal = new CalendarBuilder().build(new StringReader(fixedDefinition));
+        VTimeZone vtz = (VTimeZone)cal.getComponent(VTimeZone.VTIMEZONE);
+        TimeZone tzz = new TimeZone(vtz);
+        DateTime d = new DateTime("20100428T140000Z");
+        d.setTimeZone(tzz);
+        assertEquals("20100428T170000", d.toString());
+    }
     
     /**
      * @return
      */
     public static TestSuite suite() {
         TestSuite suite = new TestSuite();
+
+        suite.addTest(new TimeZoneTest("testMinsk", "Europe/Minsk"));
         suite.addTest(new TimeZoneTest("testGetId", "Australia/Melbourne"));
         suite.addTest(new TimeZoneTest("testGetId", "US/Mountain", "America/Denver"));
         suite.addTest(new TimeZoneTest("testGetId", "Asia/Calcutta", "Asia/Kolkata"));
@@ -356,7 +415,10 @@ public class TimeZoneTest extends TestCase {
         suite.addTest(new TimeZoneTest("testInDaylightTime", "Europe/Paris", cal.getTime(), true));
         cal.set(2010, 9, 31, 4, 0, 0);
         suite.addTest(new TimeZoneTest("testInDaylightTime", "Europe/Paris", cal.getTime(), false));
-
+        cal.set(2010, 9, 31, 4, 0, 0);
+        suite.addTest(new TimeZoneTest("testInDaylightTime", "America/Bahia", cal.getTime(), false));
+        cal.set(2002, 11, 04, 4, 0, 0);
+        suite.addTest(new TimeZoneTest("testInDaylightTime", "America/Bahia", cal.getTime(), true));
         return suite;
     }
 }
