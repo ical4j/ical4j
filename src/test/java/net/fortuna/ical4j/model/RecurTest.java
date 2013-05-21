b15a0fdb320aa0c2fd901f4e959ed7786ad43683
arnaudq <arnaudq@quillaud.org>
2013-05-21 09:49:20 +0200

    FREQ=YEARLY;BYWEEKNO test

--- src/test/java/net/fortuna/ical4j/model/RecurTest.java
+++ src/test/java/net/fortuna/ical4j/model/RecurTest.java
@@ -826,7 +826,7 @@ public class RecurTest extends TestCase {
         recur = new Recur("FREQ=YEARLY;COUNT=4;INTERVAL=2;BYMONTH=1,2,3;BYMONTHDAY=-1");
         suite.addTest(new RecurTest(recur, seed, periodStart, new DateTime("20100131T070000")));
         
-        // rrule never matching any candidate  - should reach limit
+        // rather uncommon rule
         recur = new Recur("FREQ=YEARLY;BYWEEKNO=1,2,3,4");
         suite.addTest(new RecurTest(recur, new DateTime("20130101T120000Z"),
                 new DateTime("20130101T120000Z"), new DateTime("20140101T120000Z"), Value.DATE_TIME, 4));
