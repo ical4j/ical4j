54d9b21f3a704f032211e2227733e3fcaa5e0f09
fortuna <fortuna@users.sourceforge.net>
2013-09-10 23:47:56 +1000

    Updated tzurl home

--- etc/scripts/update_zoneinfo.sh
+++ etc/scripts/update_zoneinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export TZURL_HOME=/home/build/tzurl
+export TZURL_HOME=/home/fortuna/Development/tzurl
 rsync -av $TZURL_HOME/zoneinfo src/main/resources
 rsync -av $TZURL_HOME/zoneinfo-global src/main/resources
 
