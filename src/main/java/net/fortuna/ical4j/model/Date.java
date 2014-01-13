e0f3b8d47614423212fc86ef7eb68eb726a72a72
Ben Fortuna <bfortuna@odecee.com.au>
2014-01-13 12:45:26 +1100

    Updated javadoc for ical4j 2

--- src/main/java/net/fortuna/ical4j/model/Date.java
+++ src/main/java/net/fortuna/ical4j/model/Date.java
@@ -31,60 +31,77 @@
  */
 package net.fortuna.ical4j.model;
 
+import net.fortuna.ical4j.util.CompatibilityHints;
+import net.fortuna.ical4j.util.Dates;
+import net.fortuna.ical4j.util.TimeZones;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
-import net.fortuna.ical4j.util.CompatibilityHints;
-import net.fortuna.ical4j.util.Dates;
-import net.fortuna.ical4j.util.TimeZones;
-
 
 /**
- * $Id$
- *
- * Created on 26/06/2005
- *
- * Base class for all representations of time values in RFC2445.
- *
+ * A representation of the DATE object defined in RFC5445.
+ * <p/>
+ * <strong>NOTE:</strong> iCal4j calculates the timezone of Date objects by default. In the iCalendar specification a
+ * DATE object doesn't really have a timezone associated with it, but in the ical4j implementation it does (as it is
+ * based on a java.util.Date object). An unfortunate side effect of this is that a Date object can be affected by
+ * daylight savings rules for the associated timezone. So currently ical4j offers two approaches to setting the
+ * timezone:
+ * <p/>
+ * <ol>
+ *     <li>Use UTC time (no DST rules, but depending on how the Date is constructed it may change the displayed value</li>
+ *     <li>Use local timezone (in rare cases the DST rules may affect the displayed value).</li>
+ * </ol>
+ * <p/>
+ * By default option 1 is used, but you can override this with the following flag in the ical4j.properties file:
+ * <code>net.fortuna.ical4j.timezone.date.floating=true</code>
+ * <p/>
+ * Alternatively you can avoid using the constructor that accepts a java.util.Date object, and instead provide a date
+ * string value. This should always display the correct value, however when the local timezone is used any calculations
+ * (recurrences, etc.) may still be affect by DST rules.
+ * <p/>
+ * <em>Extract from RFC5545:</em>
+ * <p/>
  * <pre>
- * 4.3.4 Date
+ * 3.3.4.  Date
  * 
- *    Value Name: DATE
+ * Value Name:  DATE
  * 
- *    Purpose: This value type is used to identify values that contain a
- *    calendar date.
+ * Purpose:  This value type is used to identify values that contain a
+ * calendar date.
  * 
- *    Formal Definition: The value type is defined by the following
- *    notation:
+ * Format Definition:  This value type is defined by the following
+ * notation:
  * 
- *      date               = date-value
+ * date               = date-value
  * 
- *      date-value         = date-fullyear date-month date-mday
- *      date-fullyear      = 4DIGIT
- *      date-month         = 2DIGIT        ;01-12
- *      date-mday          = 2DIGIT        ;01-28, 01-29, 01-30, 01-31
- *                                         ;based on month/year
+ * date-value         = date-fullyear date-month date-mday
+ * date-fullyear      = 4DIGIT
+ * date-month         = 2DIGIT        ;01-12
+ * date-mday          = 2DIGIT        ;01-28, 01-29, 01-30, 01-31
+ * ;based on month/year
  * 
- *    Description: If the property permits, multiple "date" values are
- *    specified as a COMMA character (US-ASCII decimal 44) separated list
- *    of values. The format for the value type is expressed as the [ISO
- *    8601] complete representation, basic format for a calendar date. The
- *    textual format specifies a four-digit year, two-digit month, and
- *    two-digit day of the month. There are no separator characters between
- *    the year, month and day component text.
+ * Description:  If the property permits, multiple "date" values are
+ * specified as a COMMA-separated list of values.  The format for the
+ * value type is based on the [ISO.8601.2004] complete
+ * representation, basic format for a calendar date.  The textual
+ * format specifies a four-digit year, two-digit month, and two-digit
+ * day of the month.  There are no separator characters between the
+ * year, month, and day component text.
  * 
- *    No additional content value encoding (i.e., BACKSLASH character
- *    encoding) is defined for this value type.
+ * No additional content value encoding (i.e., BACKSLASH character
+ * encoding, see Section 3.3.11) is defined for this value type.
  * 
- *    Example: The following represents July 14, 1997:
+ * Example:  The following represents July 14, 1997:
  * 
- *      19970714
+ * 19970714
  * 
  * </pre>
  * 
  * @author Ben Fortuna
+ * @version 2.0
  */
 public class Date extends Iso8601 {
 
