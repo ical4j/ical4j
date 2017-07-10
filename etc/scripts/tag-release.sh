18bc405d5056946713d1396abf215d4de7876e98
Ben Fortuna <bfortuna@odecee.com.au>
2017-07-11 00:20:37 +1000

    Support additional options passed to gradle

--- etc/scripts/tag-release.sh
+++ etc/scripts/tag-release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 GIT_BRANCH=${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
-[[ $GIT_BRANCH =~ ^release ]] && ./gradlew -Pbintray_user=$BINTRAY_USER -Pbintray_key=$BINTRAY_KEY release bintrayUpload
+[[ $GIT_BRANCH =~ ^release ]] && ./gradlew -Pbintray_user=$BINTRAY_USER -Pbintray_key=$BINTRAY_KEY release bintrayUpload $@
