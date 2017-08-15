1d3f070af4aef7896107fc134142bff4e07a2ca2
Ben Fortuna <benfortuna@gmail.com>
2017-08-15 21:40:13 +1000

    Configure sonatype credentials

--- etc/scripts/tag-release.sh
+++ etc/scripts/tag-release.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 GIT_BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
-[[ $GIT_BRANCH =~ ^release ]] && ./gradlew -Pbintray_user=$BINTRAY_USER -Pbintray_key=$BINTRAY_KEY release bintrayUpload $@
+[[ $GIT_BRANCH =~ ^release ]] && ./gradlew -Pbintray_user=$BINTRAY_USER -Pbintray_key=$BINTRAY_KEY \
+ -Psonatype_username=$SONATYPE_USER -Psonatype_password=$SONATYPE_PASSWORD \
+ release bintrayUpload $@
