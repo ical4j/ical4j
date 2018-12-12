45ff076d1bee45112d7fa113929879be31611009
Ben Fortuna <benfortuna@gmail.com>
2018-12-12 23:45:29 +1100

    Added test for demonstrating leap year behaviour (implicit BYMONTHDAY rules applied after processing this rule)

--- src/test/groovy/net/fortuna/ical4j/transform/recurrence/ByMonthRuleTest.groovy
+++ src/test/groovy/net/fortuna/ical4j/transform/recurrence/ByMonthRuleTest.groovy
@@ -2,6 +2,7 @@ package net.fortuna.ical4j.transform.recurrence
 
 import net.fortuna.ical4j.model.Date
 import net.fortuna.ical4j.model.DateList
+import net.fortuna.ical4j.model.DateTime
 import net.fortuna.ical4j.model.NumberList
 import net.fortuna.ical4j.model.parameter.Value
 import spock.lang.Specification
@@ -13,14 +14,15 @@ class ByMonthRuleTest extends Specification {
         ByMonthRule rule = [new NumberList(byMonthPart), Optional.empty()]
 
         and: 'a list of dates'
-        DateList dateList = [Value.DATE]
+        DateList dateList = [valueType]
         dateList.addAll(dates)
 
         expect: 'the rule transforms the dates correctly'
         rule.transform(dateList) == expectedResult
 
         where:
-        byMonthPart     | dates                           | expectedResult
-        '1,2'         | [new Date('20150103')]    | [new Date('20150103'), new Date('20150203')]
+        byMonthPart     | dates                                | valueType   | expectedResult
+        '1,2'         | [new Date('20150103')]          | Value.DATE  | [new Date('20150103'), new Date('20150203')]
+        '2'         | [new DateTime("20200229T000000")] | Value.DATE_TIME| [new DateTime("20210228T000000")]
     }
 }
