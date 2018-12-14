b22d38c349e0338dfa1fbc9ac3c93ec2461940aa
Ben Fortuna <benfortuna@gmail.com>
2018-12-14 19:03:09 +1100

    Implemented more complete limit/expansion rules according to RFC5545

--- src/test/groovy/net/fortuna/ical4j/transform/recurrence/ByMonthRuleTest.groovy
+++ src/test/groovy/net/fortuna/ical4j/transform/recurrence/ByMonthRuleTest.groovy
@@ -14,7 +14,7 @@ class ByMonthRuleTest extends Specification {
 
     def 'verify transformations by month'() {
         given: 'a bymonth rule'
-        ByMonthRule rule = [new NumberList(byMonthPart), Optional.empty()]
+        ByMonthRule rule = [new NumberList(byMonthPart), frequency]
 
         and: 'a list of dates'
         DateList dateList = [valueType]
@@ -24,8 +24,10 @@ class ByMonthRuleTest extends Specification {
         rule.transform(dateList) == expectedResult
 
         where:
-        byMonthPart     | dates                                | valueType   | expectedResult
-        '1,2'         | [new Date('20150103')]          | Value.DATE  | [new Date('20150103'), new Date('20150203')]
-        '2'         | [new DateTime("20200229T000000")] | Value.DATE_TIME| [new DateTime("20200229T000000")]
+        byMonthPart     | frequency     | dates                             | valueType       | expectedResult
+        '1,2'         | YEARLY | [new Date('20150103')]            | Value.DATE      | [new Date('20150103'), new Date('20150203')]
+        '2'         | YEARLY | [new DateTime("20200229T000000")] | Value.DATE_TIME | [new DateTime("20200229T000000")]
+        '10,12'         | YEARLY | [new Date("20181010")]            | Value.DATE      | [new Date("20181010"), new Date("20181210")]
+        '10,12'         | DAILY | [new Date("20181010"), new Date("20181110"), new Date("20181210")]            | Value.DATE      | [new Date("20181010"), new Date("20181210")]
     }
 }
