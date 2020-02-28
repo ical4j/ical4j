31bc07bb20faf16371bd327608f8badc7cccb9b8
Ben Fortuna <benfortuna@gmail.com>
2020-02-29 10:56:11 +1100

    Support extension component factories

--- src/main/java/net/fortuna/ical4j/data/DefaultComponentFactorySupplier.java
+++ src/main/java/net/fortuna/ical4j/data/DefaultComponentFactorySupplier.java
@@ -3,19 +3,34 @@ package net.fortuna.ical4j.data;
 import net.fortuna.ical4j.model.ComponentFactory;
 import net.fortuna.ical4j.model.component.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 
 class DefaultComponentFactorySupplier implements Supplier<List<ComponentFactory>> {
 
+    private final List<ComponentFactory> factories;
+
+    public DefaultComponentFactorySupplier(ComponentFactory... extraFactories) {
+        factories = new ArrayList<>();
+        factories.add(new Available.Factory());
+        factories.add(new Daylight.Factory());
+        factories.add(new Standard.Factory());
+        factories.add(new VAlarm.Factory());
+        factories.add(new VAvailability.Factory());
+        factories.add(new VEvent.Factory());
+        factories.add(new VFreeBusy.Factory());
+        factories.add(new VJournal.Factory());
+        factories.add(new VTimeZone.Factory());
+        factories.add(new VToDo.Factory());
+        factories.add(new VVenue.Factory());
+
+        factories.addAll(Arrays.asList(extraFactories));
+    }
+
     @Override
     public List<ComponentFactory> get() {
-        List<ComponentFactory> rfc5545 = Arrays.asList(new Available.Factory(), new Daylight.Factory(), new Standard.Factory(),
-                new VAlarm.Factory(), new VAvailability.Factory(), new VEvent.Factory(),
-                new VFreeBusy.Factory(), new VJournal.Factory(), new VTimeZone.Factory(),
-                new VToDo.Factory(), new VVenue.Factory());
-
-        return rfc5545;
+        return factories;
     }
 }
