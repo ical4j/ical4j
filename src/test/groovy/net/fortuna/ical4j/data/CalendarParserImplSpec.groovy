e428236efe8b9dd040db968a556ff17fcbbb05a8
Edward Whiting <edwhiting@bigpond.com>
2020-08-18 13:21:50 +1000

    Add superfluous newline characters to the input data for the test case which verifies parsing empty lines.  This verifies the fix for issue #409 on Github.  https://github.com/ical4j/ical4j/issues/409

--- src/test/groovy/net/fortuna/ical4j/data/CalendarParserImplSpec.groovy
+++ src/test/groovy/net/fortuna/ical4j/data/CalendarParserImplSpec.groovy
@@ -92,7 +92,7 @@ class CalendarParserImplSpec extends Specification {
 
 	def 'verify parsing empty lines'() {
 		setup:
-		String input = "BEGIN:VCALENDAR\r\n$contentLines\r\nEND:VCALENDAR"
+		String input = "BEGIN:VCALENDAR\r\n\r\n$contentLines\r\nEND:VCALENDAR"
 		compatibilityHints.each {
 			CompatibilityHints.setHintEnabled(it, true)
 		}
@@ -108,6 +108,7 @@ class CalendarParserImplSpec extends Specification {
 
 		where:
 		contentLines << ['''BEGIN:VEVENT
+
 CLASS:
 CREATED:20121015T070600Z
 DTSTART:20121018T020000Z
@@ -142,12 +143,15 @@ X-MS-OLK-CONFTYPE:0
 X-MICROSOFT-CDO-ATTENDEE-CRITICAL-CHANGE:20121015T070600Z
 X-MICROSOFT-CDO-OWNER-CRITICAL-CHANGE:20121015T070600Z
 BEGIN:VALARM
+
 ACTION:DISPLAY
 TRIGGER;VALUE=DURATION:-PT15M
 DESCRIPTION:REMINDER
 RELATED=START:-PT00H15M00S
 END:VALARM
-END:VEVENT''']
+
+END:VEVENT
+''']
 		compatibilityHints << [[KEY_RELAXED_UNFOLDING, KEY_RELAXED_PARSING]]
 	}
 }
