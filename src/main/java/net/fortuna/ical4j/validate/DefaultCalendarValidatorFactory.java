ef48c4925652b1ab8a56b7af2b72b21a202b6259
Neale Upstone <neale@nealeupstone.com>
2021-05-28 10:42:41 +0100

    Extract constant rules in DefaultCalendarValidatorFactory
    Reduces memory footprint & garbage collection.
    
    gh-496

--- src/main/java/net/fortuna/ical4j/validate/DefaultCalendarValidatorFactory.java
+++ src/main/java/net/fortuna/ical4j/validate/DefaultCalendarValidatorFactory.java
@@ -10,9 +10,13 @@ import static net.fortuna.ical4j.validate.ValidationRule.ValidationType.OneOrLes
  * Created by fortuna on 13/09/15.
  */
 public class DefaultCalendarValidatorFactory implements  CalendarValidatorFactory {
+
+    private static final ValidationRule REQUIRED_PROPERTIES_RULE = new ValidationRule(One, PRODID, VERSION);
+    private static final ValidationRule OPTIONAL_PROPERTIES_RULE = new ValidationRule(OneOrLess, CALSCALE, METHOD);
+
     @Override
     public Validator<Calendar> newInstance() {
-        return new CalendarValidatorImpl(new ValidationRule(One, PRODID, VERSION),
-                new ValidationRule(OneOrLess, CALSCALE, METHOD));
+        return new CalendarValidatorImpl(REQUIRED_PROPERTIES_RULE,
+                OPTIONAL_PROPERTIES_RULE);
     }
 }
