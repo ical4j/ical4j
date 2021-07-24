1d02319785c865f536f384f53d5a196f8c4df122
Ben Fortuna <bfortuna@odecee.com.au>
2021-07-24 10:27:04 +1000

    Added support for ignored property names

--- src/test/groovy/net/fortuna/ical4j/model/PropertyBuilderTest.groovy
+++ src/test/groovy/net/fortuna/ical4j/model/PropertyBuilderTest.groovy
@@ -1,17 +1,16 @@
 package net.fortuna.ical4j.model
 
 import net.fortuna.ical4j.model.property.Version
-import spock.lang.Ignore
 import spock.lang.Specification
 
 class PropertyBuilderTest extends Specification {
 
     def 'test build property'() {
         given: 'a property builder instance'
-        PropertyBuilder builder = []
+        PropertyBuilder builder = [Arrays.asList(new Version.Factory())]
 
         and: 'builder is initialised'
-        builder.factories(Arrays.asList(new Version.Factory())).name('version').value("2.0")
+        builder.name('version').value("2.0")
 
         when: 'build method called'
         Property p = builder.build()
@@ -20,13 +19,26 @@ class PropertyBuilderTest extends Specification {
         p == Version.VERSION_2_0
     }
 
-    @Ignore
     def 'test build invalid property'() {
         given: 'a property builder instance'
-        PropertyBuilder builder = []
+        PropertyBuilder builder = [Arrays.asList(new Version.Factory())]
 
         and: 'builder is initialised'
-        builder.factories(Arrays.asList(new Version.Factory())).name('dtend').value('20150403')
+        builder.name('dtend').value('20150403')
+
+        when: 'build method called'
+        Property p = builder.build()
+
+        then: 'an exception is thrown'
+        thrown(IllegalArgumentException)
+    }
+
+    def 'test build ignored property'() {
+        given: 'a property builder instance'
+        PropertyBuilder builder = [[], ['DTEND']]
+
+        and: 'builder is initialised'
+        builder.name('DTEND').value('20150403')
 
         when: 'build method called'
         Property p = builder.build()
