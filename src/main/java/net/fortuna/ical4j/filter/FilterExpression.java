75db1d9a76771dfcc8d6abe98220df06be7d5f29
Ben Fortuna <bfortuna@odecee.com.au>
2021-07-28 14:26:38 +1000

    Added filter expression functions

--- src/main/java/net/fortuna/ical4j/filter/FilterExpression.java
+++ src/main/java/net/fortuna/ical4j/filter/FilterExpression.java
@@ -74,42 +74,7 @@ public class FilterExpression {
     }
 
     public static FilterExpression parse(String filterExpression) {
-        FilterExpression expression = new FilterExpression();
-        Arrays.stream(filterExpression.split("\\s*and\\s*")).forEach(part -> {
-            if (part.matches("[\\w-]+\\s*>=\\s*\\w+")) {
-                String[] greaterThanEqual = part.split("\\s*>=\\s*");
-                expression.greaterThanEqual(greaterThanEqual[0], greaterThanEqual[1]);
-            } else if (part.matches("[\\w-]+\\s*<=\\s*\\w+")) {
-                String[] lessThanEqual = part.split("\\s*<=\\s*");
-                expression.lessThanEqual(lessThanEqual[0], lessThanEqual[1]);
-            } else if (part.matches("[\\w-]+\\s*=\\s*[^<>=]+")) {
-                String[] equalTo = part.split("\\s*=\\s*");
-                expression.equalTo(equalTo[0], equalTo[1]);
-            } else if (part.matches("[\\w-]+\\s*>\\s*\\w+")) {
-                String[] greaterThan = part.split("\\s*>\\s*");
-                expression.greaterThan(greaterThan[0], greaterThan[1]);
-            } else if (part.matches("[\\w-]+\\s*<\\s*\\w+")) {
-                String[] lessThan = part.split("\\s*<\\s*");
-                expression.lessThan(lessThan[0], lessThan[1]);
-            } else if (part.matches("[\\w-]+\\s+in\\s+\\[[^<>=]+]")) {
-                String[] in = part.split("\\s*in\\s*");
-                List<String> items = Arrays.asList(in[1].replaceAll("[\\[\\]]", "")
-                        .split("\\[?\\s*,\\s*]?"));
-                expression.in(in[0], items);
-            } else if (part.matches("[\\w-]+\\s+contains\\s+\".+\"")) {
-                String[] contains = part.split("\\s*contains\\s*");
-                expression.contains(contains[0], contains[1].replaceAll("^\"?|\"?$", ""));
-            } else if (part.matches("[\\w-]+\\s+exists")) {
-                String[] exists = part.split("\\s*exists");
-                expression.exists(exists[0]);
-            } else if (part.matches("[\\w-]+\\s+not exists")) {
-                String[] notExists = part.split("\\s*not exists");
-                expression.notExists(notExists[0]);
-            } else {
-                throw new IllegalArgumentException("Invalid filter expression: " + filterExpression);
-            }
-        });
-        return expression;
+        return new FilterExpressionParser().parse(filterExpression);
     }
 
     public static <T> Predicate<T> and(List<Predicate> predicates) {
@@ -135,8 +100,8 @@ public class FilterExpression {
         return p;
     }
 
-    public Predicate<Component> toComponentPredicate() {
-        Predicate<Component> p = and(equalToMap.entrySet().stream()
+    public <T extends Component> Predicate<T> toComponentPredicate() {
+        Predicate<T> p = and(equalToMap.entrySet().stream()
                 .map(e -> new PropertyEqualToRule<>(e.getKey(), e.getValue()))
                 .collect(Collectors.toList()));
         p = p.and(and(inMap.entrySet().stream()
