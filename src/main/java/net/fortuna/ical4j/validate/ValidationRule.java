22b5f900ffdd4a543224724fb76f88bdacfccb55
Ben Fortuna <bfortuna@odecee.com.au>
2021-10-23 15:38:02 +1100

    Updated docs

--- src/main/java/net/fortuna/ical4j/validate/ValidationRule.java
+++ src/main/java/net/fortuna/ical4j/validate/ValidationRule.java
@@ -4,6 +4,13 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Defines a rule for validating iCalendar content. A rule consists of a validation type (e.g. presence/absence of
+ * specific content), and one or more identifiers (names) of iCalendar content.
+ *
+ * For example, a rule might define a test for one or less DTEND properties using the "OneOrLess" validation type
+ * and "DTEND" identifier.
+ */
 public class ValidationRule implements Serializable {
 
     public enum ValidationType { None,  One, OneOrLess, OneOrMore }
