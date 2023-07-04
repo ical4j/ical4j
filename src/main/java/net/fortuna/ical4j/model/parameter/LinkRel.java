b5fbb41b7cf16738408b221709f94e53c52b328a
Ben Fortuna <fortuna@micronode.com>
2023-07-04 13:28:29 +1000

    Support standard link relation types

--- src/main/java/net/fortuna/ical4j/model/parameter/LinkRel.java
+++ src/main/java/net/fortuna/ical4j/model/parameter/LinkRel.java
@@ -34,11 +34,11 @@
 package net.fortuna.ical4j.model.parameter;
 
 import net.fortuna.ical4j.model.Content;
+import net.fortuna.ical4j.model.LinkRelationType;
 import net.fortuna.ical4j.model.Parameter;
 import net.fortuna.ical4j.model.ParameterFactory;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * <pre>
@@ -70,9 +70,18 @@ public class LinkRel extends Parameter {
         this.value = value;
     }
 
+    public LinkRel(LinkRelationType linkRelationType) {
+        super(PARAM_NAME);
+        this.value = linkRelationType.toString();
+    }
+
     public LinkRel(URI uri) {
         super(PARAM_NAME);
         this.value = uri.toASCIIString();
+    }
+
+    public LinkRelationType getLinkRelationType() {
+        return LinkRelationType.valueOf(value);
     }
 
     public URI getUri() {
