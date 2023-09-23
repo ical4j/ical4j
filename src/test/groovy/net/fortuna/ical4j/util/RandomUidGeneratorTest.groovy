12f4fa26038da8a8bb40c0d13f3a9fc96ea77653
Ben Fortuna <fortuna@micronode.com>
2023-09-23 11:03:25 +1000

    Support generating secure random UIDs

--- src/test/groovy/net/fortuna/ical4j/util/RandomUidGeneratorTest.groovy
+++ src/test/groovy/net/fortuna/ical4j/util/RandomUidGeneratorTest.groovy
@@ -14,4 +14,15 @@ class RandomUidGeneratorTest extends Specification {
         then: 'none of the UIDs are equal'
         new HashSet<>(uids).size() == uids.size()
     }
+
+    def 'test secure random UIDs'() {
+        given: 'a secure random UID generator'
+        RandomUidGenerator uidGenerator = [true]
+
+        when: 'a list of UIDs is generated'
+        def uids = (1..1000).collect {uidGenerator.generateUid()}
+
+        then: 'none of the UIDs are equal'
+        new HashSet<>(uids).size() == uids.size()
+    }
 }
