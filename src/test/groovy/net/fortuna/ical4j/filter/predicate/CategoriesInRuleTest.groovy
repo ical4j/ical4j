ea9801fdfaf96b1e241dc6ab9a848fd604191223
Ben Fortuna <fortuna@nodelogic.au>
2025-08-22 19:27:50 +1000

    Add unit tests for CategoriesInRule to validate category presence and absence logic

--- src/test/groovy/net/fortuna/ical4j/filter/predicate/CategoriesInRuleTest.groovy
+++ src/test/groovy/net/fortuna/ical4j/filter/predicate/CategoriesInRuleTest.groovy
@@ -2,7 +2,10 @@ package net.fortuna.ical4j.filter.predicate
 
 import net.fortuna.ical4j.model.component.VEvent
 import net.fortuna.ical4j.model.property.Categories
+import net.fortuna.ical4j.util.Calendars
 import spock.lang.Specification
+
+import java.util.stream.Collectors
 
 class CategoriesInRuleTest extends Specification {
 
@@ -17,5 +20,27 @@ class CategoriesInRuleTest extends Specification {
 
         expect: 'the rule evaluates to true for the event'
         rule.test(event)
+    }
+
+    def 'test categories not in rule'() {
+        given: 'a calendar'
+        def calendar = Calendars.load('src/test/resources/samples/valid/Buddhist.ics')
+
+        and: 'a rule that checks for categories not present'
+        def rule = new CategoriesInRule('personal')
+
+        expect: 'the rule evaluates to false for the event'
+        calendar.components.stream().filter(rule).collect(Collectors.toList()).empty
+    }
+
+    def 'test categories in rule'() {
+        given: 'a calendar'
+        def calendar = Calendars.load('src/test/resources/samples/valid/ArgentinaHolidays.ics')
+
+        and: 'a rule that checks for categories not present'
+        def rule = new CategoriesInRule('Public Holiday')
+
+        expect: 'the rule evaluates to false for the event'
+        !calendar.components.stream().filter(rule).collect(Collectors.toList()).empty
     }
 }
